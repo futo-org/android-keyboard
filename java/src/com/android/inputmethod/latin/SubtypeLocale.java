3bf57a5624679a20db26df912077a53b9f90ad36
Tadashi G. Takaoka <takaoka@google.com>
2012-04-03 12:50:28 +0900

    Use "No language (QWERTY)" for language agnostic QWERTY keyboard name
    Bug: 6010147
    Change-Id: I401c2e3fcd639c0e1a03e64489a0d792810caa18

--- java/src/com/android/inputmethod/latin/SubtypeLocale.java
+++ java/src/com/android/inputmethod/latin/SubtypeLocale.java
@@ -22,6 +22,13 @@ import android.content.res.Resources;
 import java.util.Locale;
 
 public class SubtypeLocale {
+    // Special language code to represent "no language".
+    /* package for test */ static final String NO_LANGUAGE = "zz";
+    // Special country code to represent "QWERTY".
+    /* package for test */ static final String QWERTY = "QY";
+
+    public static final Locale LOCALE_NO_LANGUAGE_QWERTY = new Locale(NO_LANGUAGE, QWERTY);
+
     private static String[] sExceptionKeys;
     private static String[] sExceptionValues;
 
@@ -35,18 +42,82 @@ public class SubtypeLocale {
         sExceptionValues = res.getStringArray(R.array.subtype_locale_exception_values);
     }
 
-    public static String getFullDisplayName(Locale locale) {
-        final String localeCode = locale.toString();
+    private static String lookupExceptionalLocale(String key) {
         for (int index = 0; index < sExceptionKeys.length; index++) {
-            if (sExceptionKeys[index].equals(localeCode)) {
-                final String value = sExceptionValues[index];
-                if (value.indexOf("%s") >= 0) {
-                    final String languageName = locale.getDisplayLanguage(locale);
-                    return String.format(value, languageName);
-                }
-                return value;
+            if (sExceptionKeys[index].equals(key)) {
+                return sExceptionValues[index];
             }
         }
-        return locale.getDisplayName(locale);
+        return null;
+    }
+
+    // Get Locale's full display name in its locale.
+    // For example:
+    // "fr_CH" is converted to "Français (Suisse)".
+    // "de_QY" is converted to "Deutsche (QWERTY)". (Any locale that has country code "QY")
+    // "zz_QY" is converted to "QWERTY". (The language code "zz" means "No language", thus just
+    // ends up with the keyboard layout name.)
+    public static String getFullDisplayName(Locale locale) {
+        final String key;
+        if (locale.getLanguage().equals(NO_LANGUAGE)) {
+            key = locale.getCountry();
+        } else if (locale.getCountry().equals(QWERTY)) {
+            key = "*_" + QWERTY;
+        } else {
+            key = locale.toString();
+        }
+        final String value = lookupExceptionalLocale(key);
+        if (value == null) {
+            return toTitleCase(locale.getDisplayName(locale), locale);
+        }
+        if (value.indexOf("%s") >= 0) {
+            final String languageName = toTitleCase(locale.getDisplayLanguage(locale), locale);
+            return String.format(value, languageName);
+        }
+        return value;
+    }
+
+    // Get Locale's middle display name in its locale.
+    // For example:
+    // "fr_CH" is converted to "Français".
+    // "de_QY" is converted to "Deutsche". (Any locale that has country code "QY")
+    // "zz_QY" is converted to "QWERTY". (The language code "zz" means "No language", thus just
+    // ends up with the keyboard layout name.)
+    public static String getMiddleDisplayName(Locale locale) {
+        if (NO_LANGUAGE.equals(locale.getLanguage())) {
+            return lookupExceptionalLocale(locale.getCountry());
+        } else {
+            return toTitleCase(locale.getDisplayLanguage(locale), locale);
+        }
+    }
+
+    // Get Locale's short display name in its locale.
+    // For example:
+    // "fr_CH" is converted to "Fr".
+    // "de_QY" is converted to "De". (Any locale that has country code "QY")
+    // "zz_QY" is converter to "QY". (The language code "zz" means "No language", thus just ends
+    // up with the keyboard layout name.)
+    public static String getShortDisplayName(Locale locale) {
+        if (NO_LANGUAGE.equals(locale.getLanguage())) {
+            return locale.getCountry();
+        } else {
+            return toTitleCase(locale.getLanguage(), locale);
+        }
+    }
+
+    public static String toTitleCase(String s, Locale locale) {
+        if (s.length() <= 1) {
+            // TODO: is this really correct? Shouldn't this be s.toUpperCase()?
+            return s;
+        }
+        // TODO: fix the bugs below
+        // - This does not work for Greek, because it returns upper case instead of title case.
+        // - It does not work for Serbian, because it fails to account for the "lj" character,
+        // which should be "Lj" in title case and "LJ" in upper case.
+        // - It does not work for Dutch, because it fails to account for the "ij" digraph, which
+        // are two different characters but both should be capitalized as "IJ" as if they were
+        // a single letter.
+        // - It also does not work with unicode surrogate code points.
+        return s.toUpperCase(locale).charAt(0) + s.substring(1);
     }
 }
