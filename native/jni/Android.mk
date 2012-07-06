48a436081fc6886dbeecf1f2b25fd754952f0fe1
Ken Wakasa <kwakasa@google.com>
2012-07-06 15:35:13 +0900

    Fix the build
    Change-Id: Iff324c705b46cfae27ea9b6b8dc8b0a0d56a193d

--- native/jni/Android.mk
+++ native/jni/Android.mk
@@ -68,13 +68,12 @@ endif # FLAG_DO_PROFILE
 LOCAL_MODULE := libjni_latinime_common_static
 LOCAL_MODULE_TAGS := optional
 
-ifdef ANDROID_BUILD_TOP # In the platform build system
+# In the platform build system
 include external/stlport/libstlport.mk
-else # In the unbundled build system
-LOCAL_NDK_VERSION := 7
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := stlport_static
-endif
+# In the unbundled build system
+#LOCAL_NDK_VERSION := 7
+#LOCAL_SDK_VERSION := 14
+#LOCAL_NDK_STL_VARIANT := stlport_static
 
 include $(BUILD_STATIC_LIBRARY)
 ######################################
@@ -96,13 +95,12 @@ endif # FLAG_DO_PROFILE
 LOCAL_MODULE := libjni_latinime
 LOCAL_MODULE_TAGS := optional
 
-ifdef ANDROID_BUILD_TOP # In the platform build system
+# In the platform build system
 LOCAL_STATIC_LIBRARIES += libstlport_static
-else # In the unbundled build system
-LOCAL_NDK_VERSION := 7
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := stlport_static
-endif
+# In the unbundled build system
+#LOCAL_NDK_VERSION := 7
+#LOCAL_SDK_VERSION := 14
+#LOCAL_NDK_STL_VARIANT := stlport_static
 
 include $(BUILD_SHARED_LIBRARY)
 
