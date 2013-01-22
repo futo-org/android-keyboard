d7a8fbf6a9ec8828d4b6d1c615a6c605bbe5b72e
Satoshi Kataoka <satok@google.com>
2013-01-22 17:00:43 +0900

    refactor proximity info state
    Change-Id: Iaf0c0fb7858358209fa12145777f158e0285bc27

--- native/jni/src/proximity_info_params.cpp
+++ native/jni/src/proximity_info_params.cpp
@@ -21,4 +21,8 @@ const int ProximityInfoParams::LOOKUP_RADIUS_PERCENTILE = 50;
 const int ProximityInfoParams::FIRST_POINT_TIME_OFFSET_MILLIS = 150;
 const int ProximityInfoParams::STRONG_DOUBLE_LETTER_TIME_MILLIS = 600;
 const int ProximityInfoParams::MIN_DOUBLE_LETTER_BEELINE_SPEED_PERCENTILE = 5;
+const int ProximityInfoParams::NORMALIZED_SQUARED_DISTANCE_SCALING_FACTOR_LOG_2 = 10;
+const int ProximityInfoParams::NORMALIZED_SQUARED_DISTANCE_SCALING_FACTOR =
+        1 << NORMALIZED_SQUARED_DISTANCE_SCALING_FACTOR_LOG_2;
+const float ProximityInfoParams::NOT_A_DISTANCE_FLOAT = -1.0f;
 } // namespace latinime
