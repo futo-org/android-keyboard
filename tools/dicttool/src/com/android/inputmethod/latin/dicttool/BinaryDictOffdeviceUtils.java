a94eb97413f77b1f306133dd6d5cc347f97ed24e
Yuichiro Hanada <yhanada@google.com>
2013-08-16 11:19:53 +0900

    Remove unused imports.
    Change-Id: Ib46da6efeaa8f83a9cda24c947fc2e667a1468e8

--- tools/dicttool/src/com/android/inputmethod/latin/dicttool/BinaryDictOffdeviceUtils.java
+++ tools/dicttool/src/com/android/inputmethod/latin/dicttool/BinaryDictOffdeviceUtils.java
@@ -31,8 +31,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 
 import javax.xml.parsers.ParserConfigurationException;
