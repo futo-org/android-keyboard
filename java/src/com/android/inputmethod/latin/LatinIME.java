31ff2a4335d6cea0a5ebc11268833ceb99a4fa72
Jean Chalard <jchalard@google.com>
2013-12-25 17:07:35 +0900

    [IL45] Remove lingering messages
    Bug: 8636060
    And possibly bug: 12085864
    
    Change-Id: I867036bc3d8bd255dc8ceebdd30e28b4b3dc4b2d

--- java/src/com/android/inputmethod/latin/LatinIME.java
+++ java/src/com/android/inputmethod/latin/LatinIME.java
@@ -1338,6 +1338,7 @@ public class LatinIME extends InputMethodService implements KeyboardActionListen
         private void updateBatchInput(final InputPointers batchPointers,
                 final int sequenceNumber, final boolean forEnd) {
             synchronized (mLock) {
+                mHandler.removeMessages(MSG_UPDATE_GESTURE_PREVIEW_AND_SUGGESTION_STRIP);
                 if (!mInBatchInput) {
                     // Batch input has ended or canceled while the message was being delivered.
                     return;
@@ -1374,11 +1375,14 @@ public class LatinIME extends InputMethodService implements KeyboardActionListen
         // Called on the UI thread by LatinIME.
         public void onUpdateBatchInput(final InputPointers batchPointers,
                 final int sequenceNumber) {
-            if (mHandler.hasMessages(MSG_UPDATE_GESTURE_PREVIEW_AND_SUGGESTION_STRIP)) {
-                return;
+            synchronized (mLock) {
+                if (mHandler.hasMessages(MSG_UPDATE_GESTURE_PREVIEW_AND_SUGGESTION_STRIP)) {
+                    return;
+                }
+                mHandler.obtainMessage(MSG_UPDATE_GESTURE_PREVIEW_AND_SUGGESTION_STRIP,
+                        0 /* arg1 */,sequenceNumber /* arg2 */,
+                        batchPointers /* obj */).sendToTarget();
             }
-            mHandler.obtainMessage(MSG_UPDATE_GESTURE_PREVIEW_AND_SUGGESTION_STRIP, 0 /* arg1 */,
-                    sequenceNumber /* arg2 */, batchPointers /* obj */).sendToTarget();
         }
 
         /**
@@ -1391,6 +1395,7 @@ public class LatinIME extends InputMethodService implements KeyboardActionListen
         // Called on the UI thread by LatinIME.
         public void onCancelBatchInput() {
             synchronized (mLock) {
+                mHandler.removeMessages(MSG_UPDATE_GESTURE_PREVIEW_AND_SUGGESTION_STRIP);
                 mInBatchInput = false;
             }
         }
