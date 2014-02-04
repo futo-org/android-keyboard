853e8d8869e2b7bb63a41bee031c91e350cf9d14
Tadashi G. Takaoka <takaoka@google.com>
2014-02-04 18:39:13 +0900

    Add KeySpecParser unit test
    Change-Id: Ie1c32daecefc2909e58056f515631cbd7de687f7

---
+++ tests/src/com/android/inputmethod/keyboard/internal/KeySpecParserTests.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.inputmethod.keyboard.internal;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import com.android.inputmethod.latin.Constants;
+
+@SmallTest
+public final class KeySpecParserTests extends KeySpecParserBase {
+    @Override
+    protected void assertParser(final String message, final String keySpec,
+            final String expectedLabel, final String expectedOutputText, final int expectedIcon,
+            final int expectedCode) {
+        final String keySpecResolved = mTextsSet.resolveTextReference(keySpec);
+        final String actualLabel = KeySpecParser.getLabel(keySpecResolved);
+        final String actualOutputText = KeySpecParser.getOutputText(keySpecResolved);
+        final int actualIcon = KeySpecParser.getIconId(keySpecResolved);
+        final int actualCode = KeySpecParser.getCode(keySpecResolved, mCodesSet);
+        assertEquals(message + " [label]", expectedLabel, actualLabel);
+        assertEquals(message + " [ouptputText]", expectedOutputText, actualOutputText);
+        assertEquals(message + " [icon]",
+                KeyboardIconsSet.getIconName(expectedIcon),
+                KeyboardIconsSet.getIconName(actualIcon));
+        assertEquals(message + " [code]",
+                Constants.printableCode(expectedCode),
+                Constants.printableCode(actualCode));
+    }
+}
