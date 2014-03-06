e4b742cce8910ce1eb9af7a0f4cf9163cb90da17
Tadashi G. Takaoka <takaoka@google.com>
2014-03-06 21:40:52 +0900

    Add spanish keyboard layout tests
    This CL adds, Catalan, Esperanto, Spanish, Spanish (US), and Tagalog
    keyboard tests.
    
    Bug: 13017434
    Change-Id: I0f33bfeb05300e9dd525faac982c5f08e12f1847

---
+++ tests/src/com/android/inputmethod/keyboard/layout/tests/TestsSpanish.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.inputmethod.keyboard.layout.tests;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import com.android.inputmethod.keyboard.layout.LayoutBase;
+import com.android.inputmethod.keyboard.layout.LayoutBase.EuroLayoutCustomizer;
+import com.android.inputmethod.keyboard.layout.Spanish;
+import com.android.inputmethod.keyboard.layout.expected.ExpectedKey;
+import com.android.inputmethod.keyboard.layout.expected.ExpectedKeyboardBuilder;
+import com.android.inputmethod.keyboard.layout.tests.TestsSpanishUS.SpanishUSCustomizer;
+
+import java.util.Locale;
+
+/**
+ * es: Spanish/spanish
+ */
+@SmallTest
+public class TestsSpanish extends LayoutTestsBase {
+    private static final Locale LOCALE = new Locale("es");
+    private static final LayoutBase LAYOUT = new Spanish(new SpanishESCustomizer(LOCALE));
+
+    @Override
+    LayoutBase getLayout() { return LAYOUT; }
+
+    private static class SpanishESCustomizer extends EuroLayoutCustomizer {
+        private final SpanishUSCustomizer mSpanishUSCustomizer;
+
+        public SpanishESCustomizer(final Locale locale) {
+            super(locale);
+            mSpanishUSCustomizer = new SpanishUSCustomizer(locale);
+        }
+
+        @Override
+        public ExpectedKey[] getPunctuationMoreKeys(final boolean isPhone) {
+            return mSpanishUSCustomizer.getPunctuationMoreKeys(isPhone);
+        }
+
+        @Override
+        public ExpectedKeyboardBuilder setAccentedLetters(final ExpectedKeyboardBuilder builder) {
+            return mSpanishUSCustomizer.setAccentedLetters(builder);
+        }
+    }
+}
