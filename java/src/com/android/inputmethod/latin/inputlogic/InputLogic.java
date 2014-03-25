1079665c3c017ee024a2ffdaf3488cc8c37f087a
Jean Chalard <jchalard@google.com>
2014-03-25 15:57:47 +0900

    [CB14] Implement backspace in the combiner chain
    Bug: 13622107
    Bug: 13406701
    Change-Id: I0023b398c4451253f9f717e2bd990b8a054004bc

--- java/src/com/android/inputmethod/latin/inputlogic/InputLogic.java
+++ java/src/com/android/inputmethod/latin/inputlogic/InputLogic.java
@@ -922,7 +922,7 @@ public final class InputLogic {
                 mWordComposer.reset();
                 mWordComposer.setRejectedBatchModeSuggestion(rejectedSuggestion);
             } else {
-                mWordComposer.deleteLast();
+                mWordComposer.deleteLast(inputTransaction.mEvent);
             }
             mConnection.setComposingText(getTextWithUnderline(mWordComposer.getTypedWord()), 1);
             handler.postUpdateSuggestionStrip();
