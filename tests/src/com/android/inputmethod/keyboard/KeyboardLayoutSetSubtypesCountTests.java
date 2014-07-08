b5696b4f670b5a240534bc81b106de78aba653d9
Tadashi G. Takaoka <takaoka@google.com>
2014-07-08 12:50:16 +0900

    Add Tamil keyboard
    Bug: 9978106
    Change-Id: I226e640fe453076063121176121ed5923d1ccfa1

--- tests/src/com/android/inputmethod/keyboard/KeyboardLayoutSetSubtypesCountTests.java
+++ tests/src/com/android/inputmethod/keyboard/KeyboardLayoutSetSubtypesCountTests.java
@@ -25,7 +25,7 @@ import java.util.ArrayList;
 
 @SmallTest
 public class KeyboardLayoutSetSubtypesCountTests extends KeyboardLayoutSetTestsBase {
-    private static final int NUMBER_OF_SUBTYPES = 73;
+    private static final int NUMBER_OF_SUBTYPES = 74;
     private static final int NUMBER_OF_ASCII_CAPABLE_SUBTYPES = 45;
     private static final int NUMBER_OF_PREDEFINED_ADDITIONAL_SUBTYPES = 2;
 
