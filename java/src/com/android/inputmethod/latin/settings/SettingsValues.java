a61b7245b030395451b4c42ec218a946aa95e030
Yohei Yukawa <yukawa@google.com>
2014-08-25 02:08:21 -0700

    Use more descriptive names for some internal fields
    This CL clarifies that delay parameters are specified with
    milliseconds.
    
    Change-Id: I401a60b8c09c2a04201af5b74babcdb091c07f4a

--- java/src/com/android/inputmethod/latin/settings/SettingsValues.java
+++ java/src/com/android/inputmethod/latin/settings/SettingsValues.java
@@ -50,7 +50,7 @@ public final class SettingsValues {
 
     // From resources:
     public final SpacingAndPunctuations mSpacingAndPunctuations;
-    public final int mDelayUpdateOldSuggestions;
+    public final int mDelayInMillisecondsToUpdateOldSuggestions;
     public final long mDoubleSpacePeriodTimeout;
 
     // From preferences, in the same order as xml/prefs.xml:
@@ -106,7 +106,8 @@ public final class SettingsValues {
             final InputAttributes inputAttributes) {
         mLocale = res.getConfiguration().locale;
         // Get the resources
-        mDelayUpdateOldSuggestions = res.getInteger(R.integer.config_delay_update_old_suggestions);
+        mDelayInMillisecondsToUpdateOldSuggestions =
+                res.getInteger(R.integer.config_delay_in_milliseconds_to_update_old_suggestions);
         mSpacingAndPunctuations = new SpacingAndPunctuations(res);
 
         // Store the input attributes
@@ -332,8 +333,8 @@ public final class SettingsValues {
         final StringBuilder sb = new StringBuilder("Current settings :");
         sb.append("\n   mSpacingAndPunctuations = ");
         sb.append("" + mSpacingAndPunctuations.dump());
-        sb.append("\n   mDelayUpdateOldSuggestions = ");
-        sb.append("" + mDelayUpdateOldSuggestions);
+        sb.append("\n   mDelayInMillisecondsToUpdateOldSuggestions = ");
+        sb.append("" + mDelayInMillisecondsToUpdateOldSuggestions);
         sb.append("\n   mAutoCap = ");
         sb.append("" + mAutoCap);
         sb.append("\n   mVibrateOn = ");
