d158c430708d1840796268770c5338aa79064e33
Ken Wakasa <kwakasa@google.com>
2014-11-11 03:26:38 +0900

    Just tiny cosmetic fixes in makefile
    Change-Id: I470ba8a03d70aa520311c50f5d21979f476798b7

--- common/Android.mk
+++ common/Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := latinime-common
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
