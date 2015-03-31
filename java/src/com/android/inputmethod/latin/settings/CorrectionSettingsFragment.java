76de0a8f528830d7f6c85f45f63cac0f5aa8517c
Dan Zivkovic <zivkovic@google.com>
2015-03-30 17:27:56 -0700

    Remove "Personal dictionary" link from settings.
    Bug 19966848.
    
    Change-Id: Ice6a030d2d2319822d5108c5b6a37efa4de1090f

--- java/src/com/android/inputmethod/latin/settings/CorrectionSettingsFragment.java
+++ java/src/com/android/inputmethod/latin/settings/CorrectionSettingsFragment.java
@@ -16,29 +16,20 @@
 
 package com.android.inputmethod.latin.settings;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
-import android.preference.TwoStatePreference;
 
 import com.android.inputmethod.dictionarypack.DictionarySettingsActivity;
 import com.android.inputmethod.latin.R;
-import com.android.inputmethod.latin.userdictionary.UserDictionaryList;
-import com.android.inputmethod.latin.userdictionary.UserDictionarySettings;
-
-import java.util.TreeSet;
 
 /**
  * "Text correction" settings sub screen.
  *
  * This settings sub screen handles the following text correction preferences.
- * - Personal dictionary
  * - Add-on dictionaries
  * - Block offensive words
  * - Auto-correction
@@ -67,40 +58,6 @@ public final class CorrectionSettingsFragment extends SubScreenFragment {
         final int number = pm.queryIntentActivities(intent, 0).size();
         if (0 >= number) {
             removePreference(Settings.PREF_CONFIGURE_DICTIONARIES_KEY);
-        }
-
-        final Preference editPersonalDictionary =
-                findPreference(Settings.PREF_EDIT_PERSONAL_DICTIONARY);
-        final Intent editPersonalDictionaryIntent = editPersonalDictionary.getIntent();
-        final ResolveInfo ri = USE_INTERNAL_PERSONAL_DICTIONARY_SETTINGS ? null
-                : pm.resolveActivity(
-                        editPersonalDictionaryIntent, PackageManager.MATCH_DEFAULT_ONLY);
-        if (ri == null) {
-            overwriteUserDictionaryPreference(editPersonalDictionary);
-        }
-    }
-
-    private void overwriteUserDictionaryPreference(final Preference userDictionaryPreference) {
-        final Activity activity = getActivity();
-        final TreeSet<String> localeList = UserDictionaryList.getUserDictionaryLocalesSet(activity);
-        if (null == localeList) {
-            // The locale list is null if and only if the user dictionary service is
-            // not present or disabled. In this case we need to remove the preference.
-            getPreferenceScreen().removePreference(userDictionaryPreference);
-        } else if (localeList.size() <= 1) {
-            userDictionaryPreference.setFragment(UserDictionarySettings.class.getName());
-            // If the size of localeList is 0, we don't set the locale parameter in the
-            // extras. This will be interpreted by the UserDictionarySettings class as
-            // meaning "the current locale".
-            // Note that with the current code for UserDictionaryList#getUserDictionaryLocalesSet()
-            // the locale list always has at least one element, since it always includes the current
-            // locale explicitly. @see UserDictionaryList.getUserDictionaryLocalesSet().
-            if (localeList.size() == 1) {
-                final String locale = (String)localeList.toArray()[0];
-                userDictionaryPreference.getExtras().putString("locale", locale);
-            }
-        } else {
-            userDictionaryPreference.setFragment(UserDictionaryList.class.getName());
         }
     }
 }
