767c0bf71b9c0af7f8451d7d355f6efad21a6e5b
Chieu Nguyen <cvnguyen@google.com>
2015-04-27 12:15:36 -0700

    Restore preference change update to show app icon setting.
    Bug: 20544640
    Change-Id: I6e2a26a8a5e43360dfefcc85bb46eae28f5e3867

--- java/src/com/android/inputmethod/latin/SystemBroadcastReceiver.java
+++ java/src/com/android/inputmethod/latin/SystemBroadcastReceiver.java
@@ -109,7 +109,7 @@ public final class SystemBroadcastReceiver extends BroadcastReceiver {
         context.sendBroadcast(updateIntent);
     }
 
-    private static void toggleAppIcon(final Context context) {
+    public static void toggleAppIcon(final Context context) {
         final int appInfoFlags = context.getApplicationInfo().flags;
         final boolean isSystemApp = (appInfoFlags & ApplicationInfo.FLAG_SYSTEM) > 0;
         if (Log.isLoggable(TAG, Log.INFO)) {
