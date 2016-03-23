a4e9383971924af588788a3acac4f0533bde9217
Dan Albert <danalbert@google.com>
2016-03-22 17:06:45 -0700

    Stop mixing and matching platform and NDK.
    The library was being built with the NDK, but the test is being built
    with the platform, in this case building against the NDK STL headers
    and then using the platform library, which are not ABI compatible.
    
    Bug: http://b/27801981
    Change-Id: I7386dfe6ac73334d7c1b1e0cbc8dc346c84313bf

--- native/jni/TargetUnitTests.mk
+++ native/jni/TargetUnitTests.mk
@@ -27,10 +27,8 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(LATIN_IME_SRC_DIR)
 LOCAL_MODULE := liblatinime_target_static_for_unittests
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(addprefix $(LATIN_IME_SRC_DIR)/, $(LATIN_IME_CORE_SRC_FILES))
-# Here intentionally use libc++_shared rather than libc++_static because
-# $(BUILD_NATIVE_TEST) has not yet supported libc++_static.
 LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := c++_shared
+LOCAL_NDK_STL_VARIANT := c++_static
 include $(BUILD_STATIC_LIBRARY)
 
 #################### Target native tests
@@ -44,6 +42,8 @@ LOCAL_MODULE_TAGS := tests
 LOCAL_SRC_FILES :=  \
     $(addprefix $(LATIN_IME_TEST_SRC_DIR)/, $(LATIN_IME_CORE_TEST_FILES))
 LOCAL_STATIC_LIBRARIES += liblatinime_target_static_for_unittests
+LOCAL_SDK_VERSION := 14
+LOCAL_NDK_STL_VARIANT := c++_static
 include $(BUILD_NATIVE_TEST)
 
 #################### Clean up the tmp vars
