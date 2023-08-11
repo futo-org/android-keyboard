6b42077940c5c06c2a5a5ae7aa363cb13c77bd1e
abb128 <abb128@sapples.net>
2023-08-11 23:03:20 +0300

    Add build CI

---
+++ setUpKeysCI.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo ${KEYSTORE_FILE?Need keystore file} | base64 --decode > key.jks
+echo storePassword=${KEYSTORE_PASSWORD?Need keystore password} > keystore.properties
+echo keyPassword=${KEY_PASSWORD?Need key password} >> keystore.properties
+echo keyAlias=${KEYSTORE_ALIAS?Need key alias} >> keystore.properties
+echo storeFile=key.jks >> keystore.properties
