7e31ade8fe0d5e13d649bd9c5e413a319adecd2f
Aleksandras Kostarevas <alex@futo.org>
2023-08-31 22:48:35 +0300

    Add crash reporting file to CI

--- setUpPropertiesCI.sh
+++ setUpPropertiesCI.sh
@@ -4,3 +4,5 @@ echo storePassword=${KEYSTORE_PASSWORD?Need keystore password} > keystore.proper
 echo keyPassword=${KEY_PASSWORD?Need key password} >> keystore.properties
 echo keyAlias=${KEYSTORE_ALIAS?Need key alias} >> keystore.properties
 echo storeFile=key.jks >> keystore.properties
+
+echo ${CRASHREPORTING_FILE?Need crash reporting file} | base64 --decode > crashreporting.properties
