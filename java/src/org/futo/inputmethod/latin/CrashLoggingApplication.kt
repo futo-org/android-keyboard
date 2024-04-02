2a4840d7946940ab2bd12d57588c4730496e2d9f
Aleksandras Kostarevas <aleks076@protonmail.com>
2024-04-02 13:03:48 -0500

    Switch to email crash reporting

--- java/src/org/futo/inputmethod/latin/CrashLoggingApplication.kt
+++ java/src/org/futo/inputmethod/latin/CrashLoggingApplication.kt
@@ -3,33 +3,31 @@ package org.futo.inputmethod.latin
 import android.app.Application
 import android.content.Context
 import org.acra.config.dialog
-import org.acra.config.httpSender
+import org.acra.config.mailSender
 import org.acra.data.StringFormat
 import org.acra.ktx.initAcra
-import org.acra.sender.HttpSender
 
 class CrashLoggingApplication : Application() {
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
 
-        if(BuildConfig.ENABLE_ACRA) {
-            initAcra {
-                reportFormat = StringFormat.JSON
+        initAcra {
+            reportFormat = StringFormat.JSON
 
-                dialog {
-                    text = getString(R.string.crashed_text)
-                    title = getString(R.string.crashed_title)
-                    positiveButtonText = getString(R.string.crash_report_accept)
-                    negativeButtonText = getString(R.string.crash_report_reject)
-                    resTheme = android.R.style.Theme_DeviceDefault_Dialog
-                }
+            dialog {
+                text = getString(R.string.crashed_text)
+                title = getString(R.string.crashed_title)
+                positiveButtonText = getString(R.string.crash_report_accept)
+                negativeButtonText = getString(R.string.crash_report_reject)
+                resTheme = android.R.style.Theme_DeviceDefault_Dialog
+            }
 
-                httpSender {
-                    uri = BuildConfig.ACRA_URL
-                    basicAuthLogin = BuildConfig.ACRA_USER
-                    basicAuthPassword = BuildConfig.ACRA_PASSWORD
-                    httpMethod = HttpSender.Method.POST
-                }
+            mailSender {
+                mailTo = "keyboard@futo.org"
+                reportAsFile = true
+                reportFileName = "Crash.txt"
+                subject = "Keyboard Crash Report"
+                body = "I experienced this crash. My version: ${BuildConfig.VERSION_NAME}.\n\n(Enter details here if necessary)"
             }
         }
     }
