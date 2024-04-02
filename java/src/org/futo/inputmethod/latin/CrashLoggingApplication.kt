15ba12809524f53be0f3ce1fe9fed798953f2366
Aleksandras Kostarevas <aleks076@protonmail.com>
2024-04-02 16:56:53 -0500

    Add back http crash report for now

--- java/src/org/futo/inputmethod/latin/CrashLoggingApplication.kt
+++ java/src/org/futo/inputmethod/latin/CrashLoggingApplication.kt
@@ -3,9 +3,11 @@ package org.futo.inputmethod.latin
 import android.app.Application
 import android.content.Context
 import org.acra.config.dialog
+import org.acra.config.httpSender
 import org.acra.config.mailSender
 import org.acra.data.StringFormat
 import org.acra.ktx.initAcra
+import org.acra.sender.HttpSender
 
 class CrashLoggingApplication : Application() {
     override fun attachBaseContext(base: Context?) {
@@ -22,12 +24,22 @@ class CrashLoggingApplication : Application() {
                 resTheme = android.R.style.Theme_DeviceDefault_Dialog
             }
 
-            mailSender {
-                mailTo = "keyboard@futo.org"
-                reportAsFile = true
-                reportFileName = "Crash.txt"
-                subject = "Keyboard Crash Report"
-                body = "I experienced this crash. My version: ${BuildConfig.VERSION_NAME}.\n\n(Enter details here if necessary)"
+            if(BuildConfig.ENABLE_ACRA) {
+                httpSender {
+                    uri = BuildConfig.ACRA_URL
+                    basicAuthLogin = BuildConfig.ACRA_USER
+                    basicAuthPassword = BuildConfig.ACRA_PASSWORD
+                    httpMethod = HttpSender.Method.POST
+                }
+            } else {
+                mailSender {
+                    mailTo = "keyboard@futo.org"
+                    reportAsFile = true
+                    reportFileName = "Crash.txt"
+                    subject = "Keyboard Crash Report"
+                    body =
+                        "I experienced this crash. My version: ${BuildConfig.VERSION_NAME}.\n\n(Enter details here if necessary)"
+                }
             }
         }
     }
