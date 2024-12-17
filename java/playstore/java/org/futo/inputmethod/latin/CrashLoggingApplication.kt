fe15133e3c16f15ef2e1e853f4bdd2387d7e251d
Aleksandras Kostarevas <aleks076@protonmail.com>
2024-12-17 17:25:52 +0200

    Add WorkManager config provider to Application

--- java/playstore/java/org/futo/inputmethod/latin/CrashLoggingApplication.kt
+++ java/playstore/java/org/futo/inputmethod/latin/CrashLoggingApplication.kt
@@ -2,8 +2,12 @@ package org.futo.inputmethod.latin
 
 import android.app.Application
 import androidx.datastore.preferences.core.Preferences
+import androidx.work.Configuration
 
-class CrashLoggingApplication : Application() {
+class CrashLoggingApplication : Application(), Configuration.Provider {
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder().build()
+
     companion object {
         fun logPreferences(preferences: Preferences) {
 
