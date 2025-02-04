ea329d118a93780d96f296778e39e640c067de78
Commit User <code.commit.user@gmail.com>
2025-02-04 11:56:45 +0100

    fix: formatting

--- tests/src/org/futo/inputmethod/keyboard/layout/tests/TestsUkrainian.java
+++ tests/src/org/futo/inputmethod/keyboard/layout/tests/TestsUkrainian.java
@@ -17,7 +17,7 @@
 package org.futo.inputmethod.keyboard.layout.tests;
 
 import android.test.suitebuilder.annotation.SmallTest;
-import java.util.Locale;
+
 import org.futo.inputmethod.keyboard.layout.EastSlavic;
 import org.futo.inputmethod.keyboard.layout.LayoutBase;
 import org.futo.inputmethod.keyboard.layout.Symbols;
@@ -26,65 +26,57 @@ import org.futo.inputmethod.keyboard.layout.customizer.EastSlavicCustomizer;
 import org.futo.inputmethod.keyboard.layout.expected.ExpectedKey;
 import org.futo.inputmethod.keyboard.layout.expected.ExpectedKeyboardBuilder;
 
+import java.util.Locale;
+
 /**
  * uk: Ukrainian/east_slavic
  */
 @SmallTest
 public final class TestsUkrainian extends LayoutTestsBase {
-  private static final Locale LOCALE = new Locale("uk");
-  private static final LayoutBase LAYOUT = new EastSlavic(new UkrainianCustomizer(LOCALE));
-
-  @Override
-  LayoutBase getLayout() {
-    return LAYOUT;
-  }
-
-  private static class UkrainianCustomizer extends EastSlavicCustomizer {
-    UkrainianCustomizer(final Locale locale) {
-      super(locale);
-    }
+    private static final Locale LOCALE = new Locale("uk");
+    private static final LayoutBase LAYOUT = new EastSlavic(new UkrainianCustomizer(LOCALE));
 
     @Override
-    public ExpectedKey getCurrencyKey() {
-      return CURRENCY_HRYVNIA;
-    }
+    LayoutBase getLayout() { return LAYOUT; }
 
-    @Override
-    public ExpectedKey[] getOtherCurrencyKeys() {
-      return SymbolsShifted.CURRENCIES_OTHER_GENERIC;
-    }
+    private static class UkrainianCustomizer extends EastSlavicCustomizer {
+        UkrainianCustomizer(final Locale locale) { super(locale); }
 
-    @Override
-    public ExpectedKey[] getDoubleQuoteMoreKeys() {
-      return Symbols.DOUBLE_QUOTES_R9L;
-    }
+        @Override
+        public ExpectedKey getCurrencyKey() { return CURRENCY_HRYVNIA; }
 
-    @Override
-    public ExpectedKey[] getSingleQuoteMoreKeys() {
-      return Symbols.SINGLE_QUOTES_R9L;
-    }
+        @Override
+        public ExpectedKey[] getOtherCurrencyKeys() {
+            return SymbolsShifted.CURRENCIES_OTHER_GENERIC;
+        }
 
-    // U+20B4: "₴" HRYVNIA SIGN
-    private static final ExpectedKey CURRENCY_HRYVNIA =
-        key("\u20B4", Symbols.CURRENCY_GENERIC_MORE_KEYS);
+        @Override
+        public ExpectedKey[] getDoubleQuoteMoreKeys() { return Symbols.DOUBLE_QUOTES_R9L; }
 
-    @Override
-    public ExpectedKeyboardBuilder setAccentedLetters(final ExpectedKeyboardBuilder builder) {
-      return builder
-          // U+0433: "г" CYRILLIC SMALL LETTER GHE
-          // U+0491: "ґ" CYRILLIC SMALL LETTER GHE WITH UPTURN
-          .setMoreKeysOf("\u0433", "\u0491")
-          // U+0449: "щ" CYRILLIC SMALL LETTER SHCHA
-          .replaceKeyOfLabel(EastSlavic.ROW1_9, key("\u0449", additionalMoreKey("9")))
-          // U+0456: "і" CYRILLIC SMALL LETTER BYELORUSSIAN-UKRAINIAN I
-          // U+0457: "ї" CYRILLIC SMALL LETTER YI
-          .replaceKeyOfLabel(EastSlavic.ROW2_2, key("\u0456", moreKey("\u0457")))
-          // U+0454: "є" CYRILLIC SMALL LETTER UKRAINIAN IE
-          .replaceKeyOfLabel(EastSlavic.ROW2_11, "\u0454")
-          // U+0438: "и" CYRILLIC SMALL LETTER I
-          .replaceKeyOfLabel(EastSlavic.ROW3_5, "\u0438")
-          // U+044C: "ь" CYRILLIC SMALL LETTER SOFT SIGN
-          .setMoreKeysOf("\u044C");
+        @Override
+        public ExpectedKey[] getSingleQuoteMoreKeys() { return Symbols.SINGLE_QUOTES_R9L; }
+
+        // U+20B4: "₴" HRYVNIA SIGN
+        private static final ExpectedKey CURRENCY_HRYVNIA = key("\u20B4",
+                Symbols.CURRENCY_GENERIC_MORE_KEYS);
+
+        @Override
+        public ExpectedKeyboardBuilder setAccentedLetters(final ExpectedKeyboardBuilder builder) {
+            return builder
+                    // U+0433: "г" CYRILLIC SMALL LETTER GHE
+                    // U+0491: "ґ" CYRILLIC SMALL LETTER GHE WITH UPTURN
+                    .setMoreKeysOf("\u0433", "\u0491")
+                    // U+0449: "щ" CYRILLIC SMALL LETTER SHCHA
+                    .replaceKeyOfLabel(EastSlavic.ROW1_9, key("\u0449", additionalMoreKey("9")))
+                    // U+0456: "і" CYRILLIC SMALL LETTER BYELORUSSIAN-UKRAINIAN I
+                    // U+0457: "ї" CYRILLIC SMALL LETTER YI
+                    .replaceKeyOfLabel(EastSlavic.ROW2_2, key("\u0456", moreKey("\u0457")))
+                    // U+0454: "є" CYRILLIC SMALL LETTER UKRAINIAN IE
+                    .replaceKeyOfLabel(EastSlavic.ROW2_11, "\u0454")
+                    // U+0438: "и" CYRILLIC SMALL LETTER I
+                    .replaceKeyOfLabel(EastSlavic.ROW3_5, "\u0438")
+                    // U+044C: "ь" CYRILLIC SMALL LETTER SOFT SIGN
+                    .setMoreKeysOf("\u044C");
+        }
     }
-  }
 }
