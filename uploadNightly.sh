ed730ddca360d0ce665f433e00cda52547ac8607
Aleksandras Kostarevas <aleks076@protonmail.com>
2025-03-20 21:13:13 -0500

    Fix uncached download URL for nightly

--- uploadNightly.sh
+++ uploadNightly.sh
@@ -6,7 +6,7 @@ aws configure set region $KEYBOARD_R2_DEFAULT_REGION
 aws s3 cp "$1" s3://$KEYBOARD_R2_BUCKET_NAME/nightly.apk --endpoint=$KEYBOARD_R2_ENDPOINT_URL
 
 echo "$2" > nightly_version
-echo "https://keyboard.futo.org/nightly.apk?v=$2" >> nightly_version
+echo "https://dl.keyboard.futo.org/nightly.apk?v=$2" >> nightly_version
 echo "$3" >> nightly_version
 
 aws s3 cp nightly_version s3://$KEYBOARD_R2_BUCKET_NAME/nightly_version --endpoint=$KEYBOARD_R2_ENDPOINT_URL
