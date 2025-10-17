decc11e2db5651ac1aa550428dca6e6eee4390c8
Aleksandras Kostarevas <alex@futo.org>
2025-10-17 15:29:16 +0100

    Add license notices for UT dictionaries

--- java/src/org/futo/inputmethod/latin/uix/settings/pages/credits/ThirdPartyList.kt
+++ java/src/org/futo/inputmethod/latin/uix/settings/pages/credits/ThirdPartyList.kt
@@ -35,6 +35,13 @@ val ThirdPartyList: List<ThirdPartyItem> = listOf(
         License.MIT
     ),
     TP(
+        "mozc",
+        "Japanese input powered by mozc",
+        "https://github.com/google/mozc",
+        "Copyright (c) 2010-2018, Google Inc",
+        License.BSD3
+    ),
+    TP(
         "Feather Icons",
         "Feather Icons",
         "https://feathericons.com",
@@ -91,17 +98,61 @@ val ThirdPartyList: List<ThirdPartyItem> = listOf(
         License.OFL1_1
     ),
     TP(
-        "mozc",
-        "Japanese input powered by mozc",
-        "https://github.com/google/mozc",
-        "Copyright 2010-2018, Google Inc",
-        License.BSD3
-    ),
-    TP(
         "mozc dictionaries",
         "Japanese dictionaries include data from IPAdic",
         "https://github.com/google/mozc",
-        "Copyright 2000, 2001, 2002, 2003 Nara Institute of Science and Technology.  All Rights Reserved.",
+        "Copyright (c) 2000, 2001, 2002, 2003 Nara Institute of Science and Technology.  All Rights Reserved.",
         License.NAIST_2003
+    ),
+    TP(
+        "UT Dictionary",
+        "Japanese dictionaries enriched with UT Dictionary project which includes sources listed below",
+        "https://github.com/utuhiro78/merge-ut-dictionaries",
+        "Copyright (c) utuhiro78",
+        License.Apache2
+    ),
+    TP(
+        "Wikipedia",
+        "Japanese dictionaries - Wikipedia",
+        "https://ja.wikipedia.org/wiki/Wikipedia:%E3%82%A6%E3%82%A3%E3%82%AD%E3%83%9A%E3%83%87%E3%82%A3%E3%82%A2%E3%82%92%E4%BA%8C%E6%AC%A1%E5%88%A9%E7%94%A8%E3%81%99%E3%82%8B",
+        "Copyright (c) Wikipedia",
+        License.CC_BY_SA_4_0
+    ),
+    TP(
+        "alt-cannadic",
+        "Japanese dictionaries - alt-cannadic",
+        "http://web.archive.org/https://ja.osdn.net/projects/alt-cannadic/wiki/FrontPage",
+        "Copyright (c) alt-cannadic authors",
+        License.GPL_V2
+    ),
+    TP(
+        "edict2",
+        "Japanese dictionaries - edict2",
+        "https://www.edrdg.org/wiki/index.php/JMdict-EDICT_Dictionary_Project",
+        "Copyright (c) Electronic Dictionary Research and Development Group",
+        License.CC_BY_SA_4_0
+    ),
+    TP(
+        "SudachiDict",
+        "Japanese dictionaries - SudachiDict",
+        "https://github.com/WorksApplications/SudachiDict",
+        "Copyright (c) SudachiDict authors",
+        License.Apache2
+    ),
+    TP(
+        "SKK-JISYO",
+        "Japanese dictionaries - SKK-JISYO",
+        "https://github.com/skk-dev/dict/blob/master/SKK-JISYO.L",
+        """Copyright (c) 1988-1995, 1997, 1999-2014
+Masahiko Sato <masahiko@kuis.kyoto-u.ac.jp>
+Hironobu Takahashi <takahasi@tiny.or.jp>,
+Masahiro Doteguchi, Miki Inooka,
+Yukiyoshi Kameyama <kameyama@kuis.kyoto-u.ac.jp>,
+Akihiko Sasaki, Dai Ando, Junichi Okukawa,
+Katsushi Sato and Nobuhiro Yamagishi
+NAKAJIMA Mikio <minakaji@osaka.email.ne.jp>
+MITA Yuusuke <clefs@mail.goo.ne.jp>
+SKK Development Team <skk@ring.gr.jp>""",
+        License.GPL_V2
     ),
 )
