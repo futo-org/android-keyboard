30d205719e9b2b0b40628723fd8ddf9b6df3f243
Aleksandras Kostarevas <alex@futo.org>
2025-12-19 14:40:26 +0200

    Disallow opening themes menu when device is locked

--- java/src/org/futo/inputmethod/latin/uix/actions/ThemeAction.kt
+++ java/src/org/futo/inputmethod/latin/uix/actions/ThemeAction.kt
@@ -1,12 +1,14 @@
 package org.futo.inputmethod.latin.uix.actions
 
 import android.content.Intent
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import org.futo.inputmethod.latin.R
 import org.futo.inputmethod.latin.uix.Action
 import org.futo.inputmethod.latin.uix.ActionWindow
+import org.futo.inputmethod.latin.uix.isDirectBootUnlocked
 import org.futo.inputmethod.latin.uix.settings.SettingsActivity
 import org.futo.inputmethod.latin.uix.theme.selector.ThemePicker
 
@@ -27,23 +29,29 @@ val ThemeAction = Action(
             @Composable
             override fun WindowContents(keyboardShown: Boolean) {
                 val context = LocalContext.current
-                ThemePicker({
-                    val intent = Intent()
-                    intent.setClass(context, SettingsActivity::class.java)
-                    intent.setFlags(
-                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
-                    )
-                    intent.putExtra("navDest", "themes")
-                    context.startActivity(intent)
-                }, {
-                    val intent = Intent()
-                    intent.setClass(context, SettingsActivity::class.java)
-                    intent.setFlags(
-                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
-                    )
-                    intent.putExtra("navDest", "themes")
-                    context.startActivity(intent)
-                })
+
+                val openSettingsLambda = {
+                    if(context.isDirectBootUnlocked && !manager.isDeviceLocked()) {
+                        val intent = Intent()
+                        intent.setClass(context, SettingsActivity::class.java)
+                        intent.setFlags(
+                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        )
+                        intent.putExtra("navDest", "themes")
+                        context.startActivity(intent)
+                    } else {
+                        val toast = Toast.makeText(
+                            context,
+                            context.getString(R.string.action_clipboard_manager_error_device_locked_title),
+                            Toast.LENGTH_SHORT
+                        )
+
+                        toast.show()
+
+                    }
+                }
+
+                ThemePicker({ openSettingsLambda() }, openSettingsLambda)
             }
         }
     }
